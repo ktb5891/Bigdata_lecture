2a122dbb52d112df606889abc9bea147e73bb18a
ktb5891 <68469395+ktb5891@users.noreply.github.com>
2021-02-09 17:31:40 +0900

    Add files via upload
    SQL alter, drop, insert

---
+++ kdhong/friend2.sql
@@ -0,0 +1,6 @@
+create table friend2(
+num int not null,
+name char(10),
+address char(80),
+tel char(20),
+primary key(num));
---
+++ kdhong/mem.sql
@@ -0,0 +1,17 @@
+create table mem (
+num int not null,
+id char(15) not null,
+name char(10) not null,
+sex char(1),
+post_num char(8),
+address char(80),
+tel char(20),
+age int,
+primary key(num)
+);
+insert into mem (num, id, name, sex, post_num, address, tel,age) values (1, "kh4444", "김형주", "M", "607-010", "부산시 동래구 명륜동", "764-3784", 33);
+insert into mem (num, id, name, sex, post_num, address, tel,age) values (2, 'tbkwak','곽태범','M','778-654','대구시 남구 대명동','966-7415',28);
+insert into mem (num, id, name, sex, post_num, address, tel,age) values (3, "chpark", "박철호", "M", "503-200","광주시 남구 지석동", "298-9730", 34);
+insert into mem (num, id, name, sex, post_num, address, tel,age) values (4, "parkyh", "박요한", "M", "411-300","경기도 성남시 남구 승학동", "856-2730", 24);
+insert into mem (num, id, name, sex, post_num, address, tel,age) values (5, "sjjang", "장수진", "W", "333-111","서울시 중구 인사동", "324-1243", 21);
+insert into mem (num, id, name, sex, post_num, address, tel,age) values (6, "jwchoi", "최정원", "W", "432-121","서울시 용산구 구미동", "987-1234", 23);
